39e77f19ebbf42aa9b37fd1d75405c60d497eff4
stanislasR243 <159252432+stanislasR243@users.noreply.github.com>
2026-03-06 13:46:11 +0100

    Update Bestiole.h

--- Bestiole.h
+++ Bestiole.h
@@ -3,7 +3,7 @@
 
 
 #include "UImg.h"
-
+#include "IBestiole.h" // inclure l'interface
 #include <iostream>
 
 using namespace std;
@@ -12,13 +12,13 @@ using namespace std;
 class Milieu;
 
 
-class Bestiole
+class Bestiole : public IBestiole //héritage publique de IBestiole
 {
 
 private :
-   static const double     AFF_SIZE;
-   static const double     MAX_VITESSE;
-   static const double     LIMITE_VUE;
+   static const double      AFF_SIZE;
+   static const double      MAX_VITESSE;
+   static const double      LIMITE_VUE;
 
    static int              next;
 
@@ -31,18 +31,40 @@ private :
 
    T               * couleur;
 
+   // <--- AJOUTS : Pour la gestion de la vie 
+   int               age;
+   int               maxAge;
+
 private :
    void bouge( int xLim, int yLim );
 
 public :                                           // Forme canonique :
    Bestiole( void );                               // Constructeur par defaut
    Bestiole( const Bestiole & b );                 // Constructeur de copies
-   ~Bestiole( void );                              // Destructeur
+   virtual ~Bestiole( void );                      // <--- MODIFICATION : Destructeur virtuel obligatoire                                           
                                                    // Operateur d'affectation binaire par defaut
-   void action( Milieu & monMilieu );
-   void draw( UImg & support );
+   
+   // --- Méthodes de l'interface IBestiole (OVERRIDE) ---
+   
+   void action( Milieu & monMilieu ) override;
+   void draw( UImg & support ) override;
+   
+   // Note : On utilise maintenant IBestiole dans la signature
+   bool jeTeVois( const IBestiole & b ) const override; 
 
-   bool jeTeVois( const Bestiole & b ) const;
+   // Getters de position et identité
+   double getX() const override { return x; }
+   double getY() const override { return y; }
+   double getOrientation() const override { return orientation; }
+   int getIdentite() const override { return identite; }
+
+   // Caractéristiques physiques de base (sans accessoires)
+   double getVitesse() const override { return vitesse; }
+   double getProtection() const override { return 1.0; } // Valeur neutre
+   double getCoeffCamouflage() const override { return 1.0; } // Pas camouflée
+
+   // État de santé
+   bool estMorte() const override;
 
    void initCoords( int xLim, int yLim );
 
