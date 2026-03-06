efe5606793a510119feae1acbddd9fb685a1e845
Jhusson2001 <jofrey.husson@imt-atlantique.net>
2026-03-06 12:19:45 +0100

    Update Decorator.cpp

--- Decorator.cpp
+++ Decorator.cpp
@@ -1,3 +1,23 @@
-class DecoratorJojo {
-    
+#include "Decorator.h"
+
+Decorator::Decorator(IBestiole* b) : bestiole(b) {}
+
+Decorator::~Decorator()
+{
+    delete bestiole;
+}
+
+void Decorator::action(Milieu& m)
+{
+    bestiole->action(m);
+}
+
+void Decorator::draw(UImg& img)
+{
+    bestiole->draw(img);
+}
+
+bool Decorator::jeTeVois(const IBestiole& b) const
+{
+    return bestiole->jeTeVois(b);
 }
