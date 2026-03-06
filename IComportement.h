73cc2cb9d7a5c0262112d2409800d57f2f61f191
stanislasR243 <159252432+stanislasR243@users.noreply.github.com>
2026-03-06 14:51:00 +0100

    Update IComportement.h

--- IComportement.h
+++ IComportement.h
@@ -1,5 +1,16 @@
-Interface IComportement {
-    // b : la bestiole
-    // milieu (pour accéder aux autres bestioles)
-    exécuter(Bestiole b, Milieu milieu)
-}
+#ifndef ICOMPORTEMENT_H
+#define ICOMPORTEMENT_H
+
+class IBestiole;
+class Milieu;
+
+class IComportement {
+public:
+    virtual ~IComportement() {}
+
+    // b : la bestiole qui agit (vue comme interface pour avoir ses accessoires)
+    // m : le milieu (pour voir les voisins)
+    virtual void execute(IBestiole& b, Milieu& m) = 0;
+};
+
+#endif
