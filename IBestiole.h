6649966dc126a4323dd5d4f9671c7094474526fa
stanislasR243 <159252432+stanislasR243@users.noreply.github.com>
2026-03-06 13:16:32 +0100

    Refactor IBestiole interface with new methods

--- IBestiole.h
+++ IBestiole.h
@@ -3,25 +3,31 @@
 
 #include "UImg.h"
 #include <iostream>
-#include <vector>
 
 class Milieu;
 
 class IBestiole {
 public:
+    virtual ~IBestiole() {}
+
+    // Logique de simulation
     virtual void action(Milieu& m) = 0;
     virtual void draw(UImg& img) = 0;
     virtual bool jeTeVois(const IBestiole& b) const = 0;
 
+    // Getters de base (indispensables pour les capteurs)
     virtual double getX() const = 0;
     virtual double getY() const = 0;
     virtual double getOrientation() const = 0;
+    virtual int getIdentite() const = 0; // Pour distinguer les individus
     
-    // Ajouts pour les accessoires
+    // Caractéristiques physiques (modifiables par Decorator)
     virtual double getVitesse() const = 0;
     virtual double getProtection() const = 0;
-    virtual double getCoeffCamouflage() const = 0; // Pour le camouflage
+    virtual double getCoeffCamouflage() const = 0;
     
-    virtual ~IBestiole() {}
+    // État de santé
+    virtual bool estMorte() const = 0; 
 };
+
 #endif
