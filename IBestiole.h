fa1454a0bd9992c71763c0ba2eb4f3906bfb029f
stanislasR243 <159252432+stanislasR243@users.noreply.github.com>
2026-03-06 13:06:32 +0100

    ajout dans IBestiole pour les accessoires
    Added new virtual methods for speed, protection, and camouflage coefficient.

--- IBestiole.h
+++ IBestiole.h
@@ -7,12 +7,17 @@
 
 class Milieu;
 
-class IBestiole 
-    {
-    public:
-    virtual void action(Milieu&)=0;
-    virtual void draw(UImg&)=0;
-    virtual bool jeTeVois(const IBestiole&) const =0;
-    virtual ~IBestiole() {};
+class IBestiole {
+public:
+    virtual void action(Milieu& m) = 0;
+    virtual void draw(UImg& img) = 0;
+    virtual bool jeTeVois(const IBestiole& b) const = 0;
+    
+    // Ajouts pour les accessoires
+    virtual double getVitesse() const = 0;
+    virtual double getProtection() const = 0;
+    virtual double getCoeffCamouflage() const = 0; // Pour le camouflage
+    
+    virtual ~IBestiole() {}
 };
 #endif
