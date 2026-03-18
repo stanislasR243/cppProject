bd7365fddd65dbbf45315d083a167d1210aac744
Arthur2505-ops <arthur.barbe@imt-atlantique.net>
2026-03-18 15:21:27 +0100

    Update Camouflage.cpp

--- Camouflage.cpp
+++ Camouflage.cpp
@@ -1,1 +1,9 @@
+#include "Camouflage.h"
 
+Camouflage::Camouflage(IBestiole* b, double psi)
+    : Decorator(b), coeffCamouflage(psi) {}
+
+// Surcharge du coefficient de camouflage
+double Camouflage::getCoeffCamouflage() const {
+    return coeffCamouflage;
+}
