e6b58ab465bdab04a50cb9271fc89b4cbd1caf08
Arthur2505-ops <arthur.barbe@imt-atlantique.net>
2026-03-18 15:20:08 +0100

    Update Camouflage.h

--- Camouflage.h
+++ Camouflage.h
@@ -1,1 +1,22 @@
+#ifndef CAMOUFLAGE_H
+#define CAMOUFLAGE_H
 
+#include "Decorator.h"
+
+class Camouflage : public Decorator {
+private:
+    double coeffCamouflage; // ψ : 0 = pas de camouflage, 1 = camouflage parfait
+
+public:
+    Camouflage(IBestiole* b, double psi);
+    virtual ~Camouflage() {}
+
+    // On surcharge uniquement le coeff de camouflage
+    double getCoeffCamouflage() const override;
+
+    // Les autres méthodes restent déléguées
+    void action(Milieu& m) override { bestiole->action(m); }
+    void draw(UImg& img) override { bestiole->draw(img); }
+};
+
+#endif
