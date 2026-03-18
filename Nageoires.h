03c16ee36ef627bcdeb260653798f2fd9204298b
Arthur2505-ops <arthur.barbe@imt-atlantique.net>
2026-03-18 15:03:11 +0100

    Add Nageoires class for speed decoration

--- Nageoires.h
+++ Nageoires.h
@@ -1,1 +1,22 @@
+#ifndef NAGEOIRES_H
+#define NAGEOIRES_H
 
+#include "Decorator.h"
+
+class Nageoires : public Decorator {
+private:
+    double coefficient; // ν : multiplicateur de vitesse
+
+public:
+    Nageoires(IBestiole* b, double coeff);
+    virtual ~Nageoires() {}
+
+    // Surcharge de la vitesse
+    double getVitesse() const override;
+
+    // On délègue tout le reste
+    void action(Milieu& m) override { bestiole->action(m); }
+    void draw(UImg& img) override { bestiole->draw(img); }
+};
+
+#endif
