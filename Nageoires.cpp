f598b1af0b58984e8fdef195ce3ecbd30827c34d
Arthur2505-ops <arthur.barbe@imt-atlantique.net>
2026-03-18 15:00:54 +0100

    Add Nageoire class with speed modification
    Implemented Nageoire class to modify speed of bestiole.

--- Nageoires.cpp
+++ Nageoires.cpp
@@ -1,1 +1,9 @@
+#include "Nageoire.h"
 
+Nageoire::Nageoire(IBestiole* b, double coeff)
+    : Decorator(b), coefficient(coeff) {}
+
+// On surcharge la vitesse : vitesse originale * coefficient
+double Nageoire::getVitesse() const {
+    return bestiole->getVitesse() * coefficient;
+}
