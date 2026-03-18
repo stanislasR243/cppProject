8f547a5fedc90a5d18b26a4e85398dce51d17dc1
Arthur2505-ops <arthur.barbe@imt-atlantique.net>
2026-03-18 14:47:46 +0100

    Add Oreilles class with visibility functionality

--- Oreilles.h
+++ Oreilles.h
@@ -1,1 +1,18 @@
+#ifndef OREILLES_H
+#define OREILLES_H
 
+#include "Decorator.h"
+
+class Oreilles : public Decorator {
+private:
+    double distance; // δo
+    double capacite; // γo
+
+public:
+    Oreilles(IBestiole* b, double d, double g);
+    virtual ~Oreilles() {}
+
+    bool jeTeVois(const IBestiole& autre) const override;
+};
+
+#endif
