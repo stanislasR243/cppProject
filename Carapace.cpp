18bad9ede3bce1bba84f8804c2a9461498598bb3
Arthur2505-ops <arthur.barbe@imt-atlantique.net>
2026-03-18 15:17:35 +0100

    Update Carapace.cpp

--- Carapace.cpp
+++ Carapace.cpp
@@ -1,1 +1,14 @@
+#include "Carapace.h"
 
+Carapace::Carapace(IBestiole* b, double protection, double vitesse)
+    : Decorator(b), protectionCoeff(protection), vitesseCoeff(vitesse) {}
+
+// La protection : probabilité de mourir réduite
+double Carapace::getProtection() const {
+    return bestiole->getProtection() * protectionCoeff;
+}
+
+// La vitesse : réduite par un facteur η
+double Carapace::getVitesse() const {
+    return bestiole->getVitesse() / vitesseCoeff; // facteur η > 1 ralentit
+}
